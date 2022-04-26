fd7c2f16c882f1c32828a43acebf24e31383df4c
Guilherme Almeida <iugmali@gmail.com>
2022-04-26 10:09:51 -0300

    add basket mock

---
+++ src/mocks/basket.ts
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.png";
+
+const basket = {
+  basketTop: {
+    title: "Detalhes da cesta"
+  },
+  basketDetails: {
+    name: "Cesta de Verduras",
+    farmLogo: logo,
+    farmTitle: "Jenny Jack Farm",
+    description: "Uma cesta de produtos selecionados cuidadosamente da fazenda direto para a sua cozinha",
+    price: "R$ 40,00"
+  },
+}
+export default basket;
